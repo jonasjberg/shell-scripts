2f5360090b772184cd6f6726f126481bde37d35f
Jonas Sjöberg <jomeganas@gmail.com>
2016-12-01 03:54:48 +0100

    Reformat code.

--- markdowngrep.py
+++ markdowngrep.py
@@ -11,30 +11,32 @@ import re
 
 def parseCommandLine():
     parser = argparse.ArgumentParser(prog='markdowngrep',
-                                     description='Searches markdown-formatted text files for a given pattern/string and, '
-                                                 'given a match; returns the heading that contains the text.',
-                                     epilog='')
+        description='Searches markdown-formatted text files for a given '
+                    'pattern/string and, '
+                    'given a match; returns the heading that contains the '
+                    'text.',
+        epilog='')
 
     parser.add_argument('-v', '--verbose',
-                        help="Enable verbose output. Increases information printed to stdout.",
-                        action='store_true')
+        help="Enable verbose output. Increases information printed to stdout.",
+        action='store_true')
 
     parser.add_argument('pattern',
-                        nargs='?',
-                        help='Pattern to match.')
+        nargs='?',
+        help='Pattern to match.')
 
     parser.add_argument('-i', '--input',
-                        dest='input',
-                        nargs='?',
-                        help='Files to search.',
-                        type=argparse.FileType('r'), default=sys.stdin)
+        dest='input',
+        nargs='?',
+        help='Files to search.',
+        type=argparse.FileType('r'), default=sys.stdin)
 
     args = parser.parse_args()
     return args
 
+
 def processInput(input, pattern):
     log.debug('Got input:' + str(input) + ' and pattern: ' + str(pattern))
-
 
     input_data = [line.rstrip() for line in input]
 
@@ -45,7 +47,8 @@ def processInput(input, pattern):
     for num, line in enumerate(input_data):
         if line.strip():
             if pattern in line:
-                log.debug('Found match: [{number}] {content}'.format(number=num, content=line.strip()))
+                log.debug('Found match: [{number}] {content}'.format(number=num,
+                    content=line.strip()))
 
                 # Now go backwards, starting from the matching line.
                 i = num
@@ -53,7 +56,8 @@ def processInput(input, pattern):
                     # log.debug('Searching line {}'.format(i))
                     sr_h = re.match(heading, input_data[i])
                     if sr_h:
-                        log.info('Found heading: [{heading}] ... {content}'.format(heading=sr_h.group(2), content=pattern))
+                        log.info('{h} ... {t}'.format(h=sr_h.group(2),
+                                                      t=pattern))
                         break
                     i -= 1
 
@@ -62,7 +66,8 @@ def processInput(input, pattern):
                 while i > 0:
                     sr_lh = re.match(lheading, input_data[i - 1])
                     if sr_lh:
-                        log.info('Found heading: [{heading}] ... {content}'.format(heading=sr_lh.group(1), content=pattern))
+                        log.info('{h} ... {t}'.format(h=sr_lh.group(2),
+                                                      t=pattern))
                         break
                     i -= 1
     return
@@ -72,9 +77,11 @@ if __name__ == '__main__':
     args = parseCommandLine()
 
     if args.verbose:
-        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
+        logging.basicConfig(level=logging.DEBUG,
+            format='%(asctime)s %(message)s')
     else:
-        logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+        logging.basicConfig(level=logging.INFO,
+            format='%(asctime)s %(message)s')
 
     log = logging.getLogger()
     log.info('markdowngrep started')
