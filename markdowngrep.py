8ead3501b7b788d122b6b7881bd169f20aaf54c3
Jonas Sjöberg <jomeganas@gmail.com>
2016-12-05 17:08:31 +0100

    Work in progress update of 'markdowngrep'.

--- markdowngrep.py
+++ markdowngrep.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#     ____________________________________________________________________
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#                     (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#                 GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#     ____________________________________________________________________
 
 
 import argparse
@@ -7,91 +22,145 @@ import logging
 import time
 import sys
 import re
+import pprint
 
 
-def parseCommandLine():
-    parser = argparse.ArgumentParser(prog='markdowngrep',
+def parse_commandline():
+    parser = argparse.ArgumentParser(
+        prog='markdowngrep by jonasjberg',
         description='Searches markdown-formatted text files for a given '
-                    'pattern/string and, '
-                    'given a match; returns the heading that contains the '
-                    'text.',
+                    'pattern and displays information on where, as in under '
+                    'which heading; that the pattern was found.',
         epilog='')
 
     parser.add_argument('-v', '--verbose',
-        help="Enable verbose output. Increases information printed to stdout.",
-        action='store_true')
+                        help="Enable verbose output. Increases information "
+                             "printed to stdout.",
+                        action='store_true')
 
     parser.add_argument('pattern',
-        nargs='?',
-        help='Pattern to match.')
+                        nargs='?',
+                        help='Pattern to match.')
 
     parser.add_argument('-i', '--input',
-        dest='input',
-        nargs='?',
-        help='Files to search.',
-        type=argparse.FileType('r'), default=sys.stdin)
+                        dest='input',
+                        nargs='?',
+                        help='Files to search.',
+                        type=argparse.FileType('r'), default=sys.stdin)
+
+    parser.add_argument('-t', '--top-level',
+                        dest='toplevel',
+                        action='store_true',
+                        help='Climb to closest first level heading. Default '
+                             'is closest heading, regardless of level.')
+
+    parser.add_argument()
 
     args = parser.parse_args()
     return args
 
 
-def processInput(input, pattern):
-    log.debug('Got input:' + str(input) + ' and pattern: ' + str(pattern))
+def process_input(input, pattern):
+    log.debug('Input: {}'.format(str(input)))
+    log.debug('Pattern: {}'.format(str(pattern)))
 
     input_data = [line.rstrip() for line in input]
 
-    # https://github.com/lepture/mistune/blob/master/mistune.py
-    heading = re.compile(r'^ *(#{1,6}) *([^\n]+?) *#* *(?:\n+|$)')
-    lheading = re.compile(r'^([^\n]+)\n *(=|-)+ *(?:\n+|$)')
-
+    matches = []
     for num, line in enumerate(input_data):
         if line.strip():
             if pattern in line:
-                log.debug('Found match: [{number}] {content}'.format(number=num,
-                    content=line.strip()))
+                matches += [{'line': num,
+                             'text': line.strip()}]
 
-                # Now go backwards, starting from the matching line.
-                i = num
-                while i > 0:
-                    # log.debug('Searching line {}'.format(i))
-                    sr_h = re.match(heading, input_data[i])
-                    if sr_h:
-                        log.info('{h} ... {t}'.format(h=sr_h.group(2),
-                                                      t=pattern))
-                        break
-                    i -= 1
+    for match in matches:
+        match['parents'] = find_line_parent_headings(input_data, match['line'])
 
-                # TODO: FIX THIS HACKY MESS!
-                i = num
-                while i > 0:
-                    sr_lh = re.match(lheading, input_data[i - 1])
-                    if sr_lh:
-                        log.info('{h} ... {t}'.format(h=sr_lh.group(2),
-                                                      t=pattern))
-                        break
-                    i -= 1
-    return
+        log.debug('Found match:')
+        for line in pprint.pformat(match).split('\n'):
+            log.debug(line)
+
+    return matches
+
+
+def display_results(matches):
+    max_width = 120
+    text_width = 20
+    for match in matches:
+        for parent in match['parents']:
+
+            if len(parent['text']) > text_width:
+                text_width = len(parent['text'])
+
+            if args.toplevel:
+                if parent['level'] == 1:
+                    print('{n:04d} "{t}" {m}'.format(n=parent['line'],
+                                                     t=parent['text'],
+                                                     m=match['text']))
+                    break
+            else:
+                print('{n:04d} "{t}" {m}'.format(n=parent['line'],
+                                                 t=parent['text'],
+                                                 m=match['text']))
+                break
+
+
+def find_line_parent_headings(textlines, start_line):
+    # https://github.com/lepture/mistune/blob/master/mistune.py
+    heading = re.compile(r'^ *(#{1,6}) *([^\n]+?) *#* *(?:\n+|$)')
+
+    # Detect Setext-style header. Must be first 2 lines of block.
+    # https://github.com/waylan/Python-Markdown/blob/master/markdown
+    # /blockprocessors.py
+    lheading = re.compile(r'^.*?\n[=-]+[ ]*(\n|$)', re.MULTILINE)
+
+    parents = []
+    # textlines = textlines.rsplit('\n')
+    i = start_line
+    while i >= 0:
+        sr_h = re.match(heading, textlines[i])
+        if sr_h:
+            # log.debug('Found parent on line {l} : '
+            #           '{p}'.format(l=i, p=sr_h.group(2)))
+            parents += [{'line': i,
+                         'level': len(sr_h.group(1)),
+                         'text': sr_h.group(2)}]
+
+        sr_lh = re.match(lheading, textlines[i - 1] + '\n' + textlines[i])
+        if sr_lh:
+            # log.debug('Found parent on line {l} : '
+            #           '{p}'.format(l=i, p=sr_lh.group(0)))
+            if textlines[i].startswith('='):
+                level = 1
+            else:
+                level = 2
+            parents += [{'line': i - 1,
+                         'level': level,
+                         'text': textlines[i - 1]}]
+        i -= 1
+
+    return parents
 
 
 if __name__ == '__main__':
-    args = parseCommandLine()
+    args = parse_commandline()
 
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG,
-            format='%(asctime)s %(message)s')
+                            format='%(asctime)s %(message)s')
     else:
         logging.basicConfig(level=logging.INFO,
-            format='%(asctime)s %(message)s')
+                            format='%(asctime)s %(message)s')
 
     log = logging.getLogger()
-    log.info('markdowngrep started')
+    log.debug('[STARTING]')
 
     startTime = time.time()
 
-    processInput(args.input, args.pattern)
+    results = process_input(args.input, args.pattern)
+    display_results(results)
 
     endTime = time.time()
     duration = endTime - startTime
 
-    logging.info('Elapsed time: ' + str(duration) + ' seconds')
-    logging.info('.. DONE!')
+    logging.debug('[FINISHED] Elapsed time: ' + str(duration) + ' seconds')
---
+++ markdowngrep_test.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#     ____________________________________________________________________
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#                     (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#                 GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#     ____________________________________________________________________
+
+
+from unittest import TestCase
+
+from markdowngrep import find_line_parent_headings
+
+
+class TestMarkdownGrep(TestCase):
+    def setUp(self):
+        self.sampletext = '''
+Title
+=====
+First paragraph.
+
+* List item 1
+* List item 2
+
+## First second level heading
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Sed augue nunc, ornare sit amet blandit quis, sagittis eget risus.
+
+
+### First third level heading
+Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+per inceptos himenaeos. Sed iaculis viverra mattis.
+
+Second second level heading
+---------------------------
+Aenean sagittis nulla libero, sit amet feugiat diam molestie et.
+
+
+### Second third level heading
+#### First fourth level heading
+Nullam non erat velit. Donec ullamcorper varius auctor.
+
+
+#### Second fourth level heading
+Suspendisse feugiat urna sed erat cursus finibus.
+
+
+# Final level one heading
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+'''
+
+        # Note that Title is on line 1.
+
+    def test_find_line_parent_headings(self):
+        # text = [line.rstrip('\n') for line in self.sampletext]
+        self.assertCountEqual(find_line_parent_headings(self.sampletext, 3),
+                              [{'line': 1, 'level': 1, 'text': 'Title'}])
+
+        self.assertCountEqual(find_line_parent_headings(self.sampletext, 6),
+                              [{'line': 1, 'level': 1, 'text': 'Title'}])
+
+        self.assertCountEqual(find_line_parent_headings(self.sampletext, 10),
+                              [{'line': 8, 'level': 2,
+                                'text': 'First second level heading'},
+                               {'line': 1, 'level': 1, 'text': 'Title'}])
+
+        self.assertCountEqual(find_line_parent_headings(self.sampletext, 15),
+                              [{'line': 13, 'level': 3,
+                                'text': 'First third level heading'},
+                               {'line': 8, 'level': 2,
+                                'text': 'First second level heading'},
+                               {'line': 1, 'level': 1, 'text': 'Title'}])
+
+        self.assertCountEqual(find_line_parent_headings(self.sampletext, 19),
+                              [{'line': 17, 'level': 2,
+                                'text': 'Second second level heading'},
+                               {'line': 13, 'level': 3,
+                                'text': 'First third level heading'},
+                               {'line': 8, 'level': 2,
+                                'text': 'First second level heading'},
+                               {'line': 1, 'level': 1, 'text': 'Title'}])
+
+        self.assertCountEqual(find_line_parent_headings(self.sampletext, 22),
+                              [{'line': 22, 'level': 3,
+                                'text': 'Second third level heading'},
+                               {'line': 17, 'level': 2,
+                                'text': 'Second second level heading'},
+                               {'line': 13, 'level': 3,
+                                'text': 'First third level heading'},
+                               {'line': 8, 'level': 2,
+                                'text': 'First second level heading'},
+                               {'line': 1, 'level': 1, 'text': 'Title'}])
+
+        self.assertCountEqual(find_line_parent_headings(self.sampletext, 23),
+                              [{'line': 23, 'level': 4,
+                                'text': 'First fourth level heading'},
+                               {'line': 22, 'level': 3,
+                                'text': 'Second third level heading'},
+                               {'line': 17, 'level': 2,
+                                'text': 'Second second level heading'},
+                               {'line': 13, 'level': 3,
+                                'text': 'First third level heading'},
+                               {'line': 8, 'level': 2,
+                                'text': 'First second level heading'},
+                               {'line': 1, 'level': 1, 'text': 'Title'}])
+
+        self.assertCountEqual(find_line_parent_headings(self.sampletext, 25),
+                              [{'line': 23, 'level': 4,
+                                'text': 'First fourth level heading'},
+                               {'line': 22, 'level': 3,
+                                'text': 'Second third level heading'},
+                               {'line': 17, 'level': 2,
+                                'text': 'Second second level heading'},
+                               {'line': 13, 'level': 3,
+                                'text': 'First third level heading'},
+                               {'line': 8, 'level': 2,
+                                'text': 'First second level heading'},
+                               {'line': 1, 'level': 1, 'text': 'Title'}])
+
+        self.assertCountEqual(find_line_parent_headings(self.sampletext, 28),
+                              [{'line': 27, 'level': 4,
+                                'text': 'Second fourth level heading'},
+                               {'line': 23, 'level': 4,
+                                'text': 'First fourth level heading'},
+                               {'line': 22, 'level': 3,
+                                'text': 'Second third level heading'},
+                               {'line': 17, 'level': 2,
+                                'text': 'Second second level heading'},
+                               {'line': 13, 'level': 3,
+                                'text': 'First third level heading'},
+                               {'line': 8, 'level': 2,
+                                'text': 'First second level heading'},
+                               {'line': 1, 'level': 1, 'text': 'Title'}])
+
+        self.assertCountEqual(find_line_parent_headings(self.sampletext, 32),
+                              [{'line': 31, 'level': 1,
+                                'text': 'Final level one heading'},
+                               {'line': 27, 'level': 4,
+                                'text': 'Second fourth level heading'},
+                               {'line': 23, 'level': 4,
+                                'text': 'First fourth level heading'},
+                               {'line': 22, 'level': 3,
+                                'text': 'Second third level heading'},
+                               {'line': 17, 'level': 2,
+                                'text': 'Second second level heading'},
+                               {'line': 13, 'level': 3,
+                                'text': 'First third level heading'},
+                               {'line': 8, 'level': 2,
+                                'text': 'First second level heading'},
+                               {'line': 1, 'level': 1, 'text': 'Title'}])
+
+        self.assertCountEqual(find_line_parent_headings(self.sampletext, 33),
+                              [{'line': 31, 'level': 1,
+                                'text': 'Final level one heading'},
+                               {'line': 27, 'level': 4,
+                                'text': 'Second fourth level heading'},
+                               {'line': 23, 'level': 4,
+                                'text': 'First fourth level heading'},
+                               {'line': 22, 'level': 3,
+                                'text': 'Second third level heading'},
+                               {'line': 17, 'level': 2,
+                                'text': 'Second second level heading'},
+                               {'line': 13, 'level': 3,
+                                'text': 'First third level heading'},
+                               {'line': 8, 'level': 2,
+                                'text': 'First second level heading'},
+                               {'line': 1, 'level': 1, 'text': 'Title'}])
