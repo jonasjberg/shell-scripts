0ca06985a28561404adc32c9db1b0b5e58afef8b
Jonas Sjöberg <jomeganas@gmail.com>
2017-12-26 03:53:04 +0100

    Skip directories with more than the matched file.
    Modifies 'find-equal-basename-dirname.sh' to skip certain matches.
    
    If the parent directory of the "matched" file contains something
    else than the "matched" file, the match is skipped.

--- find-equal-basename-dirname.sh
+++ find-equal-basename-dirname.sh
@@ -15,30 +15,46 @@ print_usage()
     cat >&2 <<EOF
 
 
-    Usage:  ${SELF_BASENAME} [PATH_TO_SEARCH]
+Usage:  ${SELF_BASENAME} [PATH_TO_SEARCH]
 
-    Where PATH_TO_SEARCH is the path to an existing file
-    or a directory to recursively search for files.
-    Files whose basename (without any extension) equals the
-    basename of the parent directory are considered "matches".
+Where PATH_TO_SEARCH is the path to an existing file
+or a directory to recursively search for files.
+Files whose basename (without any extension) equals the
+basename of the parent directory are considered "matches".
 
-    Commented shell commands that could rectify the situation is
-    printed for every "matched" file.
+Commented shell commands that would rectify the situation is
+printed for every "matched" file _if_ the matched directory
+only contains a single file.
+For instance, given the following paths:
 
-    For instance, given the following paths:
+    /tmp/redundancy/
+    ├── bar
+    │   ├── bar.txt
+    │   ├── baz.txt
+    │   └── foo.txt
+    └── foo
+        └── foo.txt
 
-        /foo/bar/foo.txt
-        /foo/bar/baz.txt
-        /foo/bar/bar.txt
+Only the last file satisfies the condition; the basename
+of the dirname ("bar") equals the basename of the file
+("bar.txt") _without_ the extension ("bar").
+And the directory "/foo/bar" would be empty after moving
+"/foo/bar/bar.txt" to "/foo/bar.txt".
 
-    Only the last file satisfies the condition; the basename
-    of the dirname ("bar") equals the basename of the file
-    ("bar.txt") _without_ the extension ("bar").
-    This would result in the following output:
+This would result in the following output:
 
-        # mv -nvi -- "/foo/bar/bar.txt" "/foo" && rmdir -v -- "/foo/bar"
+    # mv -nvi -- "/tmp/redundancy/foo/foo.txt" "/tmp/redundancy" && rmdir -v -- "/tmp/redundancy/foo"
 
-    NOTE:  No files are touched on disk!
+Which, if executed, would result in the following:
+
+    /tmp/redundancy/
+    ├── bar
+    │   ├── bar.txt
+    │   ├── baz.txt
+    │   └── foo.txt
+    └── foo.txt
+
+NOTE:  Nothing is written to disk! Commands are printed to stdout.
 
 EOF
 }
@@ -47,7 +63,6 @@ find_redundant_basename_dirname()
 {
     while IFS= read -r -d '' filepath
     do
-        # _abspath="$(realpath -e -- "$filepath")"
         _abspath="$filepath"
         [ -f "$_abspath" ] || continue
 
@@ -61,6 +76,12 @@ find_redundant_basename_dirname()
             _dest="$(dirname -- "$(dirname -- "$_abspath")")"
             [ -d "$_dirname" ] || { printf 'Expected a directory. Got: "%s"\n' "$_dirname" >&2 ; continue ; }
             [ -f "$_abspath" ] || { printf 'Expected a file. Got: "%s"\n' "$_abspath" >&2 ; continue ; }
+
+            if [ "$(find "$_dirname" -mindepth 1 -maxdepth 1 | wc -l)" -ne "1" ]
+            then
+                # Skip directories containing more than the matched file.
+                continue
+            fi
 
             printf '# mv -nvi -- "%s" "%s" && rmdir -v -- "%s"\n' "$_abspath" "$_dest" "$_dirname"
         fi
