cd41ee9d5af8e6f33e1d4f53797b9c55220f9712
Jonas Sjöberg <jonas@jonasjberg.com>
2019-06-09 14:20:57 +0200

    Major overhaul of 'convert-video-to-mp4.sh'.

--- convert-video-to-mp4.sh
+++ convert-video-to-mp4.sh
@@ -1,55 +1,72 @@
 #!/usr/bin/env bash
 
-# convert-video-to-mp4          Copyright (C) 2016-2017 Jonas Sjöberg
-# ~~~~~~~~~~~~~~~~~~~~          https://github.com/jonasjberg
+# convert-video-to-mp4.sh       Copyright 2016-2019 Jonas Sjöberg
+# ~~~~~~~~~~~~~~~~~~~~~~~       https://github.com/jonasjberg
 #                               http://www.jonasjberg.com
 #
-# Convert videos to mp4 using ffmpeg, motivated by reducing disk space.
-# Especially screen capture recordings in ogv- and webm-formats seem
-# to take up more space than necessary in my usage. This solves that.
+# Re-encodes videos using generic settings that should work good enough
+# for most source media, with the primary purpose of reduced disk space
+# prior to read-only archive storage.
+
+set -o errexit -o noclobber -o nounset
 
 
-set -e
-
-function handle_arg()
-{
-    src="$1"
-    dest="${1%.*}.mp4"
-    printf '%-25s : "%s"\n' "converting from source" "$src"
-    printf '%-25s : "%s"\n' "to destination"         "$dest"
-
-    # Information on ffmpeg command options used:
-    # http://askubuntu.com/questions/12182/how-can-i-convert-an-ogv-file-to-mp4
-    # http://askubuntu.com/a/470475
-    ffmpeg -i "$src" -c:v libx264 -preset veryslow -crf 22 -c:a libmp3lame \
-           -qscale:a 2 -ac 2 -ar 44100 -map_metadata 0:g -- "$dest"
-}
-
-function msg_error()
-{
-    printf '[ERROR] %s : "%s" ..\n' "$1" "$2" >&2
-}
+declare -a ffmpeg_options_global=(
+    -n  # Do not overwrite output files, and exit immediately if a specified
+        # output file already exists.
+    -loglevel warning
+)
+declare -a ffmpeg_options_input=(
+)
+declare -a ffmpeg_options_output=(
+    -c:v libx264
+    -preset veryslow
+    -crf 18
+    -c:a libmp3lame
+    -qscale:a 2
+    -ac 2
+    -ar 44100
+    -map_metadata 0:g
+)
 
 
-if ! command -v ffmpeg >/dev/null 2>&1; then
-    msg_error "Unable to continue without program" "ffmpeg"
+if ! command -v ffmpeg &>/dev/null
+then
+    printf 'This script requires "ffmpeg" ..\n'
     exit 1
 fi
+
 
 if [ $# -eq 0 ]
 then
-    printf "%-8s %s\n" "Usage:" "$(basename $0) [FILE]..."
-    printf "%-8s %s\n" "" "Specified file(s) will be converted to mp4."
-    exit 1
-else
-    for arg in "$@"
-    do
-        [ -z "$arg" ] && { msg_error "Got null argument" "$arg" ; continue ; }
-        [ -e "$arg" ] || { msg_error "Does not exist"    "$arg" ; continue ; }
-        [ -d "$arg" ] && { msg_error "Is a directory"    "$arg" ; continue ; }
-        [ -f "$arg" ] || { msg_error "Not a file"        "$arg" ; continue ; }
-        handle_arg "${arg}"
-    done
+    cat <<EOF
 
-    exit $?
+    USAGE:  $(command basename -- "$0") [FILE]...
+
+    Where FILE is one or more path(s) to video file(s) to be re-encoded.
+    The destination path is the original path without any file extension
+    and with an extra "_reencoded.mp4" appended.  FILE is skipped if the
+    destination path already exists.
+
+EOF
+    exit 0
 fi
+
+
+for arg in "$@"
+do
+    [ -e "$arg" ] || continue
+
+    if ! file_abspath="$(command realpath --canonicalize-existing -- "$arg")"
+    then
+        continue
+    fi
+
+    dest_abspath="${file_abspath%.*}_reencoded.mp4"
+    printf 'Writing to path "%s" ..\n' "$dest_abspath"
+
+    command ffmpeg "${ffmpeg_options_global[@]}" -i "$file_abspath" \
+                   "${ffmpeg_options_output[@]}" -- "$dest_abspath"
+done
+
+exit 0
