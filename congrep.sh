5c3897c5e5757507dfffb83cc6ccb24b50b3e221
terminalforlife <terminalforlife@yahoo.com>
2019-11-16 18:49:32 +0000

    Optimize, prefer Bourne Shell, and make safer
    I thought perhaps with `bash` having so many features, maybe it's
    possible for running with `sh` (or compatibility mode in bash) to be
    safer, due to there being fewer bells and whistles, so less about which
    to concern oneself.
    
    Since `grep` takes all of its options as traditional UNIX-style
    flags, I have AWK (running in POSIX and sandbox mode) only sending
    arguments through to `grep` which begin with at least one hyphen (`-`).
    I've tried to circumvent this, but had no joy, thankfully.
    
    I believe the code is cleaner without the redundant function juggling
    and convoluted logic. No longer do you need to glance up and down the
    script to find out what a single `if` statement does.
    
    If I've forgotten or broken something, or missed a compatibility
    issue, please let me know so that I can (hopefully) fix it.
    
    By the way, this is a brilliant idea for a script! I may wind up
    utilizing the code in this commit. As always, I appreciate the
    inspiration.

--- congrep.sh
+++ congrep.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 #                -~==================================~-
 #                 CONGREP --- "Context-sensitive" grep
@@ -18,10 +18,10 @@
 # If the file is not in the current working directory but this directory
 # is within a git repository, the root directory of the repository is
 # also checked for a $congrep_config_basename file.
-# Any positional arguments passed to this script are added after any
-# options read from the file.
-# If no file is found, this script effectively acts as a alias for grep.
 #
+# Any positional parameters passed to this script are added after any
+# options read from the file. If no file is found, this script
+# effectively acts as a alias for grep.
 #
 # Example configuration file contents:
 #
@@ -37,46 +37,40 @@
 #   $ grep -ir --exclude-dir={.git,.idea} -E 'fo[oO]+'
 # ______________________________________________________________________
 
-set -o noclobber -o pipefail -o errexit
+ConGrep='.local_grep_flags'
+UseColor='--color=auto'
+#UseColor='--color=never'
 
-readonly congrep_config_basename='.congrep_config'
+if [ -f "$ConGrep" ] && [ -r "$ConGrep" ]; then
+	GetFlags(){
+		awk -SP '
+			{
+				if(NR!~/^#+/){
+					sub(/#+.*$/, "")
+					for(FC=1; FC<=NF; FC++){
+						if($FC~/^-.*$/){
+							printf("%s ", $FC)
+						}
+					}
+				}
+			}
+		' "$1"
+	}
 
+	if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+		TopLVL=`git rev-parse --show-toplevel 2>&-`
 
-declare -a grepflags
-
-read_flags_from_config_in_cwd()
-{
-    # readarray options
-    #
-    #   -t  Remove a trailing newline from each line read.
-    #   -n  Copy at most count lines.  If count is 0, all lines are copied.
-    #
-    readarray -t -n 1 grepflags < "$congrep_config_basename"
-}
-
-has_git_executable() { command -v git &>/dev/null ; }
-has_local_config_in_cwd() { [ -f "$congrep_config_basename" ] ; }
-cwd_is_within_git_repository() { git status &>/dev/null ; }
-cd_to_git_repository_root() { cd "$(git rev-parse --show-toplevel)" ; }
-
-
-if has_local_config_in_cwd
-then
-    read_flags_from_config_in_cwd
-elif has_git_executable && cwd_is_within_git_repository
-then
-    if cd_to_git_repository_root
-    then
-        has_local_config_in_cwd && read_flags_from_config_in_cwd
-        cd - >/dev/null
-    fi
-fi
-
-if [ -n "$grepflags" ]
-then
-    # Prevent expansion of metacharacters like '{' by running with eval.
-    eval command grep "${grepflags[@]}" "$@"
+		if [ -n "$TopLVL" ]; then
+			eval grep $UseColor `GetFlags "$TopLVL/$ConGrep"` "$@"
+			exit $?
+		else
+			exit 7
+		fi
+	else
+		eval grep $UseColor `GetFlags "$ConGrep"` "$@"
+		exit $?
+	fi
 else
-    # Direct pass-through.
-    command grep "$@"
+	grep $UseColor "$@"
+	exit $?
 fi
