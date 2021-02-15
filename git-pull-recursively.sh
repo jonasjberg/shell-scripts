f2203fee934b7d3bae64d2edd821dd8af0e975e9
Jonas Sjöberg <jonas@jonasjberg.com>
2021-02-15 19:25:28 +0100

    Various fixes to 'git-pull-recursively.sh'

--- git-pull-recursively.sh
+++ git-pull-recursively.sh
@@ -1,49 +1,55 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -o noclobber -o nounset -o pipefail
 
 # git-pull-recursively.sh
 # ~~~~~~~~~~~~~~~~~~~~~~~
 # Written by Jonas Sjöberg in 2015
 # Rewritten by Jonas Sjöberg 2017-05-03
 # Updated by Jonas Sjöberg 2018-01-16
+# Updated by Jonas Sjöberg 2021-02-15
 
 # Does a recursive search for ".git" directories, enters the parent directory
 # within a subshell and performs a bunch of tests for unstaged/uncommitted
 # changes. If the tests pass, the reposity is updated using git pull if no
 # merge is needed ('--ff-only' flag).
 
-set -o noclobber -o nounset -o pipefail
-
-
 # Recursively search and perform "git pull" on all found repositories here.
-SEARCH_PATH="${HOME}/dev/sourcecode/"
+SEARCH_PATH="${1:-${HOME}/dev/sourcecode/}"
 IGNORE_LIST_BASENAME='git-pull-recursively.ignores'
 
 
-COLRED="$(tput setaf 1)"
-COLGREEN="$(tput setaf 2)"
-COLYELLOW=$(tput setaf 3)
-COLRESET="$(tput sgr0)"
+if [ -n "${TERM:-}" ] && command -v tput >/dev/null
+then
+    COLOR_RED="$(tput setaf 1)"
+    COLOR_GREEN="$(tput setaf 2)"
+    COLOR_YELLOW="$(tput setaf 3)"
+    COLOR_RESET="$(tput sgr0)"
+else
+    COLOR_RED=''
+    COLOR_GREEN=''
+    COLOR_YELLOW=''
+    COLOR_RESET=''
+fi
 
-SELF_DIRNAME="$(realpath -e -- "$(dirname -- "$0")")"
+SELF_DIRNAME="$(readlink -- "$(dirname -- "$0")")"
 IGNORE_LIST_PATH="${SELF_DIRNAME}/${IGNORE_LIST_BASENAME}"
 
-count_skipped=0
-count_failed=0
-count_total=0
-count_success=0
-
+declare -i count_skipped=0
+declare -i count_failed=0
+declare -i count_total=0
+declare -i count_success=0
 
 log_colorlabel()
 {
     local -r _color="$1" ; shift
     local -r _label="$1" ; shift
 
-    printf '%s[%s]%s %s\n' "$_color" "$_label" "$COLRESET" "$*"
+    printf '%s[%s]%s %s\n' "$_color" "$_label" "$COLOR_RESET" "$*"
 }
 
 log_warn()
 {
-    log_colorlabel "$COLYELLOW" 'WARNING' "$1"
+    log_colorlabel "$COLOR_YELLOW" 'WARNING' "$1"
     shift
 
     while test "$#" -gt "0"
@@ -61,95 +67,85 @@ log_skip()
 
 log_fail()
 {
-    log_colorlabel "$COLRED" 'FAILURE' "$*"
+    log_colorlabel "$COLOR_RED" 'FAILURE' "$*"
     count_failed="$((count_failed + 1))"
 }
 
 log_ok()
 {
-    log_colorlabel "$COLGREEN" 'SUCCESS' "$*"
+    log_colorlabel "$COLOR_GREEN" 'SUCCESS' "$*"
     count_success="$((count_success + 1))"
 }
 
 
-
 if [ ! -d "$SEARCH_PATH" ]
 then
-    log_colorlabel "$COLRED" 'FAILURE' "Invalid path: \"${SEARCH_PATH}\""
-    log_colorlabel "$COLRED" 'FAILURE' 'Variable "SEARCH_PATH" must be set before running this script'
+    log_colorlabel "$COLOR_RED" 'FAILURE' "Invalid path: \"${SEARCH_PATH}\""
+    log_colorlabel "$COLOR_RED" 'FAILURE' 'Variable "SEARCH_PATH" must be set before running this script'
     exit 1
 fi
 
 
-START_DIR="$(pwd)"
-
-while IFS= read -r -d '' repo
+while IFS= read -r -d '' _repo_gitdir
 do
     count_total="$((count_total + 1))"
 
-    _repo_dir="$(realpath -- "${repo}/..")"
-    if ! cd "${_repo_dir}"
-    then
-        log_fail "Unable to cd to directory: \"${_repo_dir}\""
-        continue
-    fi
+    _repo_worktree="$(dirname -- "$_repo_gitdir")"
+    _repo_basename="$(basename -- "$_repo_worktree")"
 
-
-    _name="$(basename "$(pwd)")"
     if [ -f "$IGNORE_LIST_PATH" ]
     then
-        if grep --fixed-strings --file="$IGNORE_LIST_PATH" -- <<< "$_name" >/dev/null
+        if grep --fixed-strings --file="$IGNORE_LIST_PATH" -- <<< "$_repo_basename" &>/dev/null
         then
-            log_skip "Ignored repository: \"${_name}\""
+            log_skip "Ignored repository: \"${_repo_basename}\""
             continue
         fi
     fi
 
-    if ! ( git status 2>&1 ) >/dev/null
+    if ! git --work-tree "$_repo_worktree" --git-dir "$_repo_gitdir" status &>/dev/null
     then
-        log_fail "Not a git repository: \"${_repo_dir}\""
+        log_fail "Not a Git repository: \"${_repo_worktree}\""
         continue
     fi
 
-    if ! git diff-files --quiet
+    if ! git --work-tree "$_repo_worktree" --git-dir "$_repo_gitdir" diff-files --quiet
     then
-        log_skip "The Repository working tree has changes that could be staged" \
-                 "Skipping repository: \"${_name}\""
+        log_skip 'The Repository working tree has changes that could be staged' \
+            "Skipping repository: \"${_repo_basename}\""
         continue
     fi
 
-    if ! git diff-index --quiet --cached HEAD
+    if ! git --work-tree "$_repo_worktree" --git-dir "$_repo_gitdir" diff-index --quiet --cached HEAD
     then
-        log_skip "Repository has staged (uncommitted) changes" \
-                 "Skipping repository: \"${_name}\""
+        log_skip 'Repository has staged (uncommitted) changes' \
+            "Skipping repository: \"${_repo_basename}\""
         continue
     fi
 
-    if git pull --ff-only --quiet
+    if echo git --work-tree "$_repo_worktree" --git-dir "$_repo_gitdir" pull --ff-only --quiet
     then
-        log_ok "Updated repository: \"${_name}\""
+        log_ok "Updated repository: \"${_repo_basename}\""
     else
-        log_fail "pull failed for repository: \"${_name}\""
+        log_fail "pull failed for repository: \"${_repo_basename}\""
     fi
 
     # Random pause between requests ..
-    sleep $(( ( RANDOM % 3 )  + 1 ))
+    sleep $(((RANDOM % 3) + 1))
 
-done < <(find "$SEARCH_PATH" -xdev -type d -name ".git" -print0)
+done < <(find "$SEARCH_PATH" -xdev -type d -name '.git' -print0)
 
 
-cd "$START_DIR"
-
-duration="$(printf '%02d hours %02d minutes %02d seconds' \
-            $(($SECONDS/3600)) $(($SECONDS%3600/60)) $(($SECONDS%60)))"
+duration="$(
+    printf '%02d hours %02d minutes %02d seconds' \
+        $((SECONDS / 3600)) $((SECONDS % 3600 / 60)) $((SECONDS % 60))
+)"
 
 printf '\n\n'
-log_colorlabel "$COLGREEN" 'FINISHED' "Elapsed time: ${duration}"
+log_colorlabel "$COLOR_GREEN" 'FINISHED' "Elapsed time: ${duration}"
 printf '           Found Total : %d\n' "$count_total"
 printf '           Updated     : %d\n' "$count_success"
 printf '           Skipped     : %d\n' "$count_skipped"
 printf '           FAILED      : %d\n' "$count_failed"
-printf '\n'
 
 if [ "$count_failed" -gt "0" ]
 then
@@ -157,4 +153,3 @@ then
 else
     exit 0
 fi
-
